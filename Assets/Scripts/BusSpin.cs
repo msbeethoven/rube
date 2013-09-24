b6eddbe8fe99d0dc20768807407dcbd72b5028ff
Core <seorisachs@gmail.com>
2013-09-24 02:48:10 -0400

    rubegoldbergass

---
+++ Assets/Scripts/BusSpin.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BusSpin : MonoBehaviour {
+	
+	public float rotateSpeed = 900f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+		{
+		if ( Time.time > 20f && Time.time < 80f) {
+		transform.Rotate( 0f, rotateSpeed, 0f ); 
+	}
+}
+}
---
+++ Assets/Scripts/CameraMove.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMove : MonoBehaviour {
+
+	Vector3 Camera1 = new Vector3(27.17879f, 4.373298f, -20.57001f);
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		if (Time.time >= 3 && Time.time < 6) {
+			transform.position = new Vector3 (27.17879f, 4.373298f, -20.57001f);
+			transform.LookAt (Camera1);
+
+		} 
+	}
+}
